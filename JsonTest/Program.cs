32c68fd85e82b79634ba2793f23c8ad77561a802
GET Academy <terje@kolderup.net>
2020-06-02 10:26:41 +0200

    Update Program.cs

--- JsonTest/Program.cs
+++ JsonTest/Program.cs
@@ -4,17 +4,25 @@ using Newtonsoft.Json;
 
 namespace JsonTest
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-          var json = DummyApi.GetSampleJson();
-          var obj = JsonConvert.DeserializeObject<IssuesResponse>(json);
-          foreach (var issue in obj.issues)
-          {
-            var issueId = issue.id;
+      // 1: kalle api: hente issues
+      // 2: for hver issue:
+      // 3: gjøre om json-string til objekt: var obj = JsonConvert.DeserializeObject<???>(json);
+      // 4: bruke objekt til å hente ut id
+      // 5: nytt api-kall etter kommentarene med id
+      // 6: gjøre om json-string til objekt: var obj = JsonConvert.DeserializeObject<??>(json);
+      // 7: bruke objekt til å hente kommentarene - bruke til det dere skal bruke til
 
-          }
-        }
+      var json = DummyApi.GetSampleJson();
+      var obj = JsonConvert.DeserializeObject<IssuesResponse>(json);
+      foreach (var issue in obj.issues)
+      {
+        var issueId = issue.id;
+
+      }
     }
+  }
 }
